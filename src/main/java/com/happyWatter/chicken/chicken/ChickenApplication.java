e7a68077ee4d76ae1132f6ad47966a02e13660be
김희수 <heesukim@kakao.com>
2021-02-06 00:33:08 +0900

    #init

---
+++ src/main/java/com/happyWatter/chicken/chicken/ChickenApplication.java
@@ -0,0 +1,13 @@
+package com.happyWatter.chicken.chicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happyWatter/chicken/chicken/ChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyWatter.chicken.chicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
